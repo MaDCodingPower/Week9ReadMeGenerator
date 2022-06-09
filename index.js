83d7cab0c216cde018901540ec2adceed97fdc99
Matthew Davis <matthewdavis1372@yahoo.com>
2022-06-09 06:13:44 -0400

    added prompts

--- index.js
+++ index.js
@@ -1,15 +1,82 @@
 // TODO: Include packages needed for this application
+
 const generateMarkdown = require("./utils/generateMarkdown")
 const inquirer = require("inquirer")
 const fs = require("fs")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your project Title?",
+    "Please provide a brief description of your application",
+    "Please provide brief instructions on installing your application",
+    "Please provide brief instructions on how to use your application",
+    "Please list the github profiles of all your collaborators",
+    "Please provide a brief description on how to test you application",
+    "Which of these licenses would you like your application to be under?",
+    "What is your Github Username?",
+    "Where can someone contact you through email for further questions?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(
+        [
+            {
+                type: "input",
+                message: questions[0],
+                name: "title"
+            },
+            {
+              type: "input",
+              message: questions[1],
+              name: "description"  
+            },
+            {
+                type: "input",
+                message: questions[2],
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: questions[3],
+                name: "usage"
+            },
+            {
+                type: "input",
+                message: questions[4],
+                name: "collaborators"
+            },
+            {
+                type: "input",
+                message: questions[5],
+                name: "test",
+            },
+            {
+                type: "list",
+                message: questions[6],
+                name: "license",
+                choices: ["BSD", "MIT", "GPL"],
+            },
+            {
+                type: "list",
+                message: questions[7],
+                name: "github"
+            },
+            {
+                type: "list",
+                message: questions[8],
+                name: "email"
+            }
+        ]
+    )
+}
 
 // Function call to initialize app
 init();
+
+
+
+
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
